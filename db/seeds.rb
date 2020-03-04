04d6d3e37c498673c646439f10262c160d8d0cef
Garron <garronvgj11@gmail.com>
2020-03-03 18:33:00 -0600

    Game seed complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,32 @@
 
 require_relative '../ar.rb'
 
-Game.destroy_all
+# Game.destroy_all
+# g_csv = File.read(Rails.root.join('lib', 'seeds', 'steam_games.csv'))
+# game_csv = CSV.parse(g_csv, headers: true, encoding: 'ISO-8859-1')
+# game_csv.each do |row|
+#   game = Game.new
+#   game.name = row['name']
+#   game.developer = row['developer']
+#   game.genre = row['genre']
+#   game.description = row['desc_snippet']
+#   game.save
+#   puts "#{game.name}, #{game.developer} saved"
+# end
+# puts "Created #{Game.count} Games."
 
-g_csv = File.read(Rails.root.join('lib', 'seeds', 'steam_games.csv'))
-game_csv = CSV.parse(g_csv, headers: true, encoding: 'ISO-8859-1')
+Genre.destroy_all
+ge_csv = File.read(Rails.root.join('lib', 'seeds', 'steam_games.csv'))
+genre_csv = CSV.parse(ge_csv, headers: true, encoding: 'ISO-8859-1')
 game_csv.each do |row|
   game = Game.new
   game.name = row['name']
   game.developer = row['developer']
   game.genre = row['genre']
   game.description = row['desc_snippet']
+  game.save
+  puts "#{game.name}, #{game.developer} saved"
 end
-
 puts "Created #{Game.count} Games."
+
+
