ca76ec9dfe269b5e04dbc8d50d3191a7776fec30
Garron <garronvgj11@gmail.com>
2020-03-03 19:15:59 -0600

    Added comments in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,18 +16,15 @@ require_relative '../ar.rb'
 # end
 # puts "Created #{Game.count} Games."
 
-Genre.destroy_all
-ge_csv = File.read(Rails.root.join('lib', 'seeds', 'steam_games.csv'))
-genre_csv = CSV.parse(ge_csv, headers: true, encoding: 'ISO-8859-1')
-game_csv.each do |row|
-  game = Game.new
-  game.name = row['name']
-  game.developer = row['developer']
-  game.genre = row['genre']
-  game.description = row['desc_snippet']
-  game.save
-  puts "#{game.name}, #{game.developer} saved"
-end
-puts "Created #{Game.count} Games."
+# Comment.destroy_all
+# c_csv = File.read(Rails.root.join('lib', 'seeds', 'metacritic_game_user_comments.csv'))
+# comment_csv = CSV.parse(c_csv, headers: true, encoding: 'ISO-8859-1')
+# comment_csv.each do |row|
+#   comment = Comment.new
+#   comment.game_title = row['Title']
+#   comment.comment_text = row['Comment']
+#   comment.save
+#   puts "#{comment.game_title} saved"
+# end
 
-
+puts "Created #{Comment.count} Comments."
